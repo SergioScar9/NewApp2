ab50d6bac3275fb88ac025f04781df8ea71fcd32
SergioScar9 <sergioscar94@gmail.com>
2025-06-13 17:55:31 +0200

    agg img

--- MyApp2/src/HomePage.jsx
+++ MyApp2/src/HomePage.jsx
@@ -1,5 +1,8 @@
 import BasicExample from "./cardHome";
 import Home from "./Home";
+import categorieImg from "./assets/Img/es.categorie2.png";
+import cardImg from "./assets/Img/es.card1.png";
+import watchImg from "./assets/Img/watch1.png";
 
 const HomePage = () => {
   return (
@@ -9,18 +12,18 @@ const HomePage = () => {
         <BasicExample
           title="Scegli la categoria"
           description="Questo è il primo passo per capire la vostra compatibilità, scegli la tua categoria preferita"
-          imgSrc="./public/img/es.categorie2.png"
+          imgSrc={categorieImg}
         />
         <BasicExample
           title="SKIP o WATCH"
           description="Scegli se passare il film visualizzato o mettere un WATCH,se anche qualcun'altro come te ha fatto click su WATCH, farete un match e saprete subito che film o serie guardare"
-          imgSrc="./public/img/es.card1.png"
+          imgSrc={cardImg}
         />
 
         <BasicExample
           title="WATCH"
           description="Ora che avete fatto WATCH-MATCH, godetevi il vostro film o serie TV insieme."
-          imgSrc="./public/img/watch1.png"
+          imgSrc={watchImg}
         />
       </div>
     </>
